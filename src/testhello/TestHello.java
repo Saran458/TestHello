4ddd3aaa2093c5dfe0a133b19eb69c239ef0aaa4
Saran458 <saran.za097gmail.com>
2022-03-25 14:44:12 +0700

    fouth commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -15,8 +15,11 @@ public class TestHello {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Hello World");
         System.out.println("Welcome to Thailand");
+        System.out.println("1 2 3 4");
+        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
     
 }
